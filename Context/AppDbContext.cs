e0b23b1a1648a6834eb50710f34f965d4ae333e5
wanderson648 <testepedroca@gmail.com>
2023-09-27 11:32:52 -0300

    Criando arquivo de contexto

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+
+    }
+}
--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -6,5 +6,8 @@
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
 
+        public ICollection<Produto>? produtos { get; set; }
+
+
     }
 }
