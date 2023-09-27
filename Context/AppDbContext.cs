6698d70940903014484d3126e4d94cbda95c814b
wanderson648 <testepedroca@gmail.com>
2023-09-27 11:48:32 -0300

    Configurando as propriedades e relacionamentos com a Fluent API

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -12,5 +12,29 @@ namespace CatalogoApi.Context
         public DbSet<Produto>? Produtos { get; set; }
         public DbSet<Categoria>? Categorias { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            // categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            // produto
+            mb.Entity<Produto>().HasKey(p => p.ProdutoId);
+            mb.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+
+            // relacionamento
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                .WithMany(p => p.Produtos)
+                .HasForeignKey(c => c.CategoriaId);
+            
+            
+        }
+
     }
 }
--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -6,8 +6,6 @@
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
 
-        public ICollection<Produto>? produtos { get; set; }
-
-
+        public ICollection<Produto>? Produtos { get; set; }
     }
 }
